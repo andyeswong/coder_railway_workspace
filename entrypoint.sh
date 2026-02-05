7297611780ec42b4e1ab17a36f06e16109b96aa3
Andres Wong <andres@enteracloud.com>
2026-02-05 00:43:34 -0800

    fix :

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,4 +13,4 @@ echo "CODER_DERP_FORCE_WEBSOCKETS: $CODER_DERP_FORCE_WEBSOCKETS"
 
 # Execute the init script
 # The init script will handle downloading and starting the agent
-eval "$CODER_AGENT_INIT_SCRIPT"
+eval "$(code-server --no-auth --port 13337)"

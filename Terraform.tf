3b7dc5f9daffb6471332952de93442921b89b8a7
Andres Wong <andres@enteracloud.com>
2026-02-05 00:27:37 -0800

    fix

--- Terraform.tf
+++ Terraform.tf
@@ -46,41 +46,8 @@ resource "railway_service" "codeserver" {
   name         = "coder-${lower(data.coder_workspace.me.name)}-${lower(data.coder_workspace_owner.me.name)}"
   project_id   = "64211a28-8a9b-4f5b-a740-deabe67017e1"
   source_image = "andyeswong/coder-workspace:stable"
+  env_vars = {
+    "CODER_AGENT_INIT_SCRIPT" = coder_agent.main.init_script
+  }
 }
 
-
-resource "railway_variable" "codeserver_agent_token" {
-  environment_id = "c7b6f1b3-bfcc-4e21-ab76-fe900a62d432"
-  service_id     = railway_service.codeserver.id
-  name           = "CODER_AGENT_TOKEN"
-  value          = coder_agent.main.token
-  
-  depends_on = [railway_service.codeserver]
-}
-
-resource "railway_variable" "codeserver_derp_force" {
-  environment_id = "c7b6f1b3-bfcc-4e21-ab76-fe900a62d432"
-  service_id     = railway_service.codeserver.id
-  name           = "CODER_DERP_FORCE_WEBSOCKETS"
-  value          = "1"
-  
-  depends_on = [railway_variable.codeserver_agent_token]
-}
-
-resource "railway_variable" "codeserver_draining_seconds" {
-  environment_id = "c7b6f1b3-bfcc-4e21-ab76-fe900a62d432"
-  service_id     = railway_service.codeserver.id
-  name           = "RAILWAY_DEPLOYMENT_DRAINING_SECONDS"
-  value          = "30"
-  
-  depends_on = [railway_variable.codeserver_derp_force]
-}
-
-resource "railway_variable" "codeserver_init_script" {
-  environment_id = "c7b6f1b3-bfcc-4e21-ab76-fe900a62d432"
-  service_id     = railway_service.codeserver.id
-  name           = "CODER_AGENT_INIT_SCRIPT"
-  value          = coder_agent.main.init_script
-  
-  depends_on = [railway_variable.codeserver_draining_seconds]
-}
